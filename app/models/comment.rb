d7eabb59ac38abd0e56d1b34a91a21c1794e3fc7
oliburt <oliburt@gmail.com>
2019-09-08 00:08:11 +0100

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].empty?
+      user = User.find_or_create_by(user_attributes)
+      self.user = user
+      self.save
+    end
+  end
 
 end
